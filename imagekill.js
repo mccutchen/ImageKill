fd262e415b6964411d7bbba0ad325753ba503ad6
Will McCutchen <mccutchen@gmail.com>
2010-05-06 11:12:00 -0500

    Use the chrome.tabs.insertCSS method (which I overlooked previously) to kill <img> elements and CSS backgrounds.

--- imagekill.js
+++
@@ -1,7 +0,0 @@
-(function() {
-    var els = document.getElementsByTagName('img');
-    els = Array.prototype.slice.call(els, 0);
-    els.forEach(function(el) {
-        el.style.display = 'none';
-    });
-})();
